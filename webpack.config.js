e79987133b74ecebc39bf822e3ab06b8001935e8
Ivan <ivandaho@gmail.com>
2017-01-13 15:01:57 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        path: __dirname,
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: "babel",
+        }]
+    }
+};
